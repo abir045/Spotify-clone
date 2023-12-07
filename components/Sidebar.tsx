ded670e324969e72e3494e79eed1eaa63d23ba35
unknown <abir045@gmail.com>
2023-12-07 14:11:52 +0600

    updated header

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,7 @@ import {HiHome} from "react-icons/hi" ;
 import {BiSearch} from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
-
+import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode
@@ -51,7 +51,7 @@ const Sidebar : React.FC<SidebarProps> = ({children}) => {
             </Box>
 
             <Box className="overflow-y-auto h-full">
-            Song Library
+            <Library />
             </Box>
 
         </div>
